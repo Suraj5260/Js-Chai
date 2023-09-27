60f188ecee5503b5ad4b6529754db5ed37096e0d
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-09-27 19:48:02 +0530

    0

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,5 @@
+const myArr = [1, 2, 3, 4]
+const arr = new Array(1, 2, 3, 4)
+
+console.log(myArr);
+console.log(arr);
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructer: "hitesh"
+}
+
+const { courseInstructer: instructer } = course
+
+console.log(instructer)
+
+
+
