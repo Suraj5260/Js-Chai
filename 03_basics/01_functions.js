e9fc568d3095f9dd1c126d3938ad8bf329645010
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-09-29 19:39:19 +0530

    arrow

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+// const yoo = function () {
+//     let username = "nomo"
+//     console.log(this)
+// }
+
+// yoo()
+
+// const chai = () => {
+//     let username = "zoro"
+//     console.log(this)
+// }
+
+// console.log(chai());
+
