50b04eb2212722f71e31ee65e8a3fde61f19c3ef
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-10-17 17:50:06 +0530

    added project

--- 07_projects/5-keyboard/chaiaurcode.js
+++ 07_projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,19 @@
-console.log('Project 5');
+const insert = document.querySelector("#insert")
+window.addEventListener("keydown", (e) => {
+    insert.innerHTML = `
+    <div class = "color">
+    <table>
+        <tr>
+            <th>key</th>
+            <th>Key Code</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${e.key === " " ? "Space" : e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+    </table>
+    </div>
+    `
+})
